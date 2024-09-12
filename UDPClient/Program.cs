11b70daaeb706edf43810e309c3c23efb6e2537b
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-09-12 11:14:44 +0200

    Add project files.

---
+++ UDPClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Net.Sockets;
+using System.Text;
+
+Console.WriteLine("UDP Client");
+
+using (UdpClient socket = new UdpClient())
+{
+    while (true)
+    { 
+    string message = Console.ReadLine();
+    byte[] data = Encoding.UTF8.GetBytes(message);
+    socket.Send(data, data.Length, "127.0.0.1", 5005);
+    }
+
+}
