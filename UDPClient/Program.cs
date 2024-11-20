02bb8f19aca5017bb2355b78c539f39416d4263c
Tan Ti Khuu <tak002@edu.zealand.dk>
2024-11-20 06:33:51 +0100

    Tilføjet kommentar

--- UDPClient/Program.cs
+++ UDPClient/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Se https://aka.ms/new-console-template for mere information
 
 using System.Net.Sockets;
 using System.Text;
@@ -8,10 +8,14 @@ Console.WriteLine("UDP Client");
 using (UdpClient socket = new UdpClient())
 {
     while (true)
-    { 
-    string message = Console.ReadLine();
-    byte[] data = Encoding.UTF8.GetBytes(message);
-    socket.Send(data, data.Length, "127.0.0.1", 5005);
-    }
+    {
+        // Læs besked fra konsollen
+        string message = Console.ReadLine();
 
+        // Konverter besked til byte array
+        byte[] data = Encoding.UTF8.GetBytes(message);
+
+        // Send data til serveren på IP-adresse 127.0.0.1 og port 5005
+        socket.Send(data, data.Length, "127.0.0.1", 5005);
+    }
 }
